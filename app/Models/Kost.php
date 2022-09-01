e095e455d0fb9ef34408d398dbeedd775fd3c073
totoprayogo1916 <totoprayogo1916@gmail.com>
2022-09-01 15:36:35 +0700

    kost -> Kost

---
+++ app/Models/Kost.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kost extends Model
+{
+    protected $table = 'kosts';
+    protected $guarded = ['id'];
+
+    public function Category(){
+        return $this->belongsTo(Category::class, 'id_category', 'id_category');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
